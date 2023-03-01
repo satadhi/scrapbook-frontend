e89f4f62aaef04e69f1a4d03795b4d189e17f6b7
satadhi <haldersatadhi@gmail.com>
2023-03-02 00:40:22 +0530

    added redux

---
+++ src/state/index.js
@@ -0,0 +1,62 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+
+    mode: 'dark',
+    user: null,
+    token: null,
+    posts: []
+}
+
+const mainSlice = createSlice({
+    name: "main",
+    initialState,
+    reducers: {
+
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+
+        setLogout: (state) => {
+            state = initialState;
+        },
+
+        setAddFriend: (state, action) => {
+
+            if (state.user)
+                state.user.friends = [...state.user.friends, action.payload.friend];
+        },
+
+        setRemoveFriend: (state, action) => {
+
+            if (state.user)
+                state.user.friends =
+                    state.user.friends.filter(ele => ele._id !== action.payload.friendId);
+        },
+
+        setCreatePost: (state, action) => {
+            state.posts = [...state.posts, action.payload.post]
+        },
+
+        setUpdatePost: (state, action) => {
+            const updatedPosts = state.posts
+                .map(post => post._id === action.payload.post._id ? action.payload.post : post);
+            state.posts = updatedPosts;
+
+        },
+
+        setDeletePost: (state, action) => {
+            const updatedPosts = state.posts
+                .map(post => post._id === action.payload.post._id ? action.payload.post : post);
+            state.posts = updatedPosts;
+
+        }
+
+    }
+
+})
+
+
+export const {setLogin,setLogout, setAddFriend, setRemoveFriend, setCreatePost, setUpdatePost, setDeletePost}
+ = mainSlice.actions;
+export default  mainSlice.reducer;
